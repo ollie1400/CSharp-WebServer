1ebe8c351291f8f8b9f171c19e55c5a8b4025055
ollie1400 <otlg123@gmail.com>
2016-01-17 12:54:24 +0000

    Initial commit.  Working state.  Uses unbuffered Socket, but commented code uses buffered TcpSocket

---
+++ WebServer/HTTPHeaderParser.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServer
+{
+    public abstract class HTTPHeaderParser
+    {
+        public static HTTPHeader Parse(string header)
+        {
+            HTTPHeader ret = new HTTPHeader();
+
+            // split into lines
+            string[] lines = header.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+            // request line is first
+            string[] parts = lines[0].Split(' ');
+            ret.Method = parts[0];
+            ret.RequestURI = parts[1];
+            ret.HTTPVersionString = parts[2];
+
+            // go through the rest of the strings and get data from them
+            // other header fields are in 5.3 of https://www.w3.org/Protocols/rfc2616/rfc2616-sec5.html
+            for (int i = 1; i < lines.Length; i++)
+            {
+                // if the line doen't contain a colon then finish
+                if (lines[i].IndexOf(':') < 0) break;
+
+                // get header field name and value
+                parts = lines[i].Split(':');
+                string fieldName = parts[0];
+                string value = parts[1].Trim();
+
+                // add into the dictionary
+                ret.HeaderFields.Add(fieldName, value);
+
+                // what is it? (case-insensitive https://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html#sec4.2)
+                if (fieldName.Equals("Accept", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    ret.Headers.Accept = value.Split(',').Select(s => s.Trim()).ToArray();
+                }
+                else if (fieldName.Equals("Accept-Charset", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    ret.Headers.AcceptCharset = value.Split(',').Select(s => s.Trim()).ToArray();
+                }
+                else if (fieldName.Equals("Accept-Encoding", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    ret.Headers.AcceptEncoding = value.Split(',').Select(s => s.Trim()).ToArray();
+                }
+                else if (fieldName.Equals("Accept-Language", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    ret.Headers.AcceptLanguage = value.Split(',').Select(s => s.Trim()).ToArray();
+                }
+                else if (fieldName.Equals("Authorization", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    ret.Headers.Authorization = value;
+                }
+                else if (fieldName.Equals("Expect", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    ret.Headers.Expect = value;
+                }
+                else if (fieldName.Equals("From", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    ret.Headers.From = value;
+                }
+                else if (fieldName.Equals("Host", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    ret.Headers.Host = value;
+                }
+                else if (fieldName.Equals("If-Match", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    ret.Headers.IfMatch = value;
+                }
+                else if (StrComp(fieldName, "Cache-Control"))
+                {
+                    ret.Headers.CacheControl = CacheControlParser.Parse(value);
+                }
+                else if (StrComp(fieldName, "User-Agent"))
+                {
+                    ret.Headers.UserAgent = value;
+                }
+                else if (StrComp(fieldName, "Connection"))
+                {
+                    ret.Headers.Connection = value;
+                }
+                else if (StrComp(fieldName, "Content-Length"))
+                {
+                    ret.Headers.ContentLength = int.Parse(value);
+                }
+            }
+
+            return ret;
+        }
+
+
+        private static bool StrComp(string one, string two)
+        {
+            return one.Equals(two, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+
+    public class HTTPHeader
+    {
+        public string Method { get; set; }
+        public string RequestURI { get; set; }
+        public string HTTPVersionString { get; set; }
+        public string Host { get; set; }
+        public Dictionary<string, string> HeaderFields { get; set; }
+
+        public RequestHeaders Headers { get; set; }
+
+        public HTTPHeader()
+        {
+            HeaderFields = new Dictionary<string, string>();
+            Headers = new RequestHeaders();
+        }
+
+        public class RequestHeaders
+        {
+            public string[] Accept { get; set; }
+            public string[] AcceptCharset { get; set; }
+            public string[] AcceptEncoding { get; set; }
+            public string[] AcceptLanguage { get; set; }
+            public string Authorization { get; set; }
+            public string Expect { get; set; }
+            public string From { get; set; }
+            public string Host { get; set; }
+            public string IfMatch { get; set; }
+            public string UserAgent { get; set; }
+            public string Connection { get; set; }
+            public int? ContentLength { get; set; }
+            public CacheControlStruct CacheControl { get; set; }
+
+        }
+
+    }
+
+
+    public abstract class CacheControlParser
+    {
+        public static CacheControlStruct Parse (string line)
+        {
+            CacheControlStruct ret = new CacheControlStruct();
+
+            // split by comma
+            string[] parts = line.Split(',').Select(s => s.Trim()).ToArray();
+            for (int i=0; i<parts.Length; i++)
+            {
+                // split by equals if there is
+                string[] bits = parts[i].Split('=');
+                if (StrComp(bits[0], "no-cache"))
+                {
+                    ret.NoCache = true;
+                } else if  (StrComp(bits[0], "no-store"))
+                {
+                    ret.NoStore = true;
+                }
+                else if (StrComp(bits[0], "max-age"))
+                {
+                    if (bits.Length > 1) ret.MaxAge = int.Parse(bits[1]);
+                }
+                else if (StrComp(bits[0], "max-stale"))
+                {
+                    ret.MaxStaleAccept = true;
+                    if (bits.Length > 1) ret.MaxStaleValue = int.Parse(bits[1]);
+                }
+                else if (StrComp(bits[0], "min-fresh"))
+                {
+                    if (bits.Length > 1) ret.MinFresh = int.Parse(bits[1]);
+                }
+                else if (StrComp(bits[0], "no-transform"))
+                {
+                    ret.NoTransform = true;
+                }
+                else if (StrComp(bits[0], "only-if-cached"))
+                {
+                    ret.OnlyIfCached = true;
+                }
+            }
+
+            return ret;
+        }
+
+        private static bool StrComp(string one, string two)
+        {
+            return one.Equals(two, StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+
+    public struct CacheControlStruct
+    {
+        public bool? NoCache { get; set; }
+        public bool? NoStore { get; set; }
+        public int? MaxAge { get; set; }
+        public bool? MaxStaleAccept { get; set; }
+        public int? MaxStaleValue { get; set; }
+        public int? MinFresh { get; set; }
+        public bool? NoTransform { get; set; }
+        public bool? OnlyIfCached { get; set; }
+    }
+}
---
+++ WebServer/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using System.Web;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Http.Headers;
+namespace WebServer
+{
+    static class Program
+    {
+        static private bool quit = false;
+        static private int port = 1425;
+        static private TcpListener tcpListener;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+
+            // set up port
+            tcpListener = new TcpListener(IPAddress.Loopback, port);
+            tcpListener.ExclusiveAddressUse = false;
+            tcpListener.Start();
+
+            // start listening loop
+            while (!quit)
+            {
+                //TcpClient socket = tcpListener.AcceptTcpClient();
+                Socket socket = tcpListener.AcceptSocket();
+                ThreadPool.QueueUserWorkItem(ServeRequest, socket);
+            }
+        }
+
+        // serve request
+        private static void ServeRequest(object context)
+        {
+            Socket socket = (Socket)context;
+            //TcpClient socket = (TcpClient)context;
+            //Stream inputStream = new BufferedStream(socket.GetStream());
+            //StreamWriter outputStream = new StreamWriter(new BufferedStream(socket.GetStream()));
+
+            byte[] buffer = new byte[1024];
+            //inputStream.Read(buffer, 0, buffer.Length);
+            socket.Receive(buffer);
+
+            string request = System.Text.Encoding.UTF8.GetString(buffer);
+            HTTPHeader header = HTTPHeaderParser.Parse(request);
+
+            // form response
+            string responseBody = "<!DOCTYPE html><html><body>Hello Chrome!! The time is " + DateTime.Now.ToShortTimeString() + "</body></html>";
+
+            string responseString = "HTTP/1.1 200 OK\r\n";
+            responseString += "Connection: close\r\n";
+            responseString += "Content-Type: text/html\r\n";
+            responseString += "Content-Length: " + responseBody.Length.ToString() + "\r\n";
+            responseString += "Date: " + DateTime.Today.ToString("R") + "\r\n";
+            responseString += "\r\n";
+            responseString += responseBody;
+
+            byte[] responseBytes = System.Text.Encoding.UTF8.GetBytes(responseString);
+            socket.Send(responseBytes);
+
+            //outputStream.WriteLine("HTTP/1.1 200 OK");
+            //outputStream.WriteLine("Content-Type: text/html");
+            //outputStream.WriteLine("Content - Length: " + responseBody.Length.ToString());
+            //outputStream.WriteLine("Date: " + DateTime.Now.ToString("R"));
+            //outputStream.WriteLine("");
+            //outputStream.WriteLine(responseBody);
+
+            //outputStream.Flush();
+            //inputStream = null;
+            //outputStream = null;
+            socket.Close();
+        }
+    }
+}
